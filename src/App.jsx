4951b46730e9e13bccfff5be8c9b821fefaecd09
christof <christof@jazab.com>
2023-04-18 17:18:06 -0700

    added Billing, Business, Button, CardDeal and hero

--- src/App.jsx
+++ src/App.jsx
@@ -35,5 +35,4 @@ const App = () => {
     </div>
   )
 }
-7
 export default App
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-    return (
-        <div>CardDeal</div>
-    )
-}
+const CardDeal = () => (
+    <section className={layout.section}>
+        <div className={layout.sectionInfo}>
+            <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden' /></h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>I'm in the business of misery let's take it from the top she's got a body like an hourglass, it's ticking like a clock.</p>
+            <Button styles='mt-10' />
+        </div>
+
+        <div className={layout.sectionImg}>
+            <img src={card} alt="card" className='w-[100%] h-[100%]' />
+
+        </div>
+    </section>
+)
 
 export default CardDeal
